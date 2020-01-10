3d9eb8abba2f2cda6156da5d71ec48dc728a457b
Fieszl Bence <bence.fieszl@outlook.hu>
2020-01-10 20:54:27 +0100

    Alapprogram feltöltése
    Alap weboldal program Flask keretrendszerrel

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template,request
+
+
+app = Flask(__name__)
+
+
+@app.route('/' ,methods=['GET', 'POST'])
+def hello_world():
+    '''if request.method == 'POST':
+            if request.form['submit_button'] == 'Do Something':
+                a=request.form['szam']'''
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run()
